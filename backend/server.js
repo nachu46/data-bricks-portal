d30b1526c412ed6864ac6cf99b5d9fdcb80ea46d
nazuk <nachunazuk@gmail.com>
2026-02-17 16:18:25 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const cors = require("cors");
 // Import routes
 const accessRoutes = require("./routes/access");
 const adminRoutes = require("./routes/admin");
+const dataAccess = require("./routes/dataAccess");
+
+
+
 
 const app = express();
 
@@ -14,6 +18,7 @@ app.use(express.json());
 // Register API routes
 app.use("/api/access", accessRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/data-access", dataAccess);
 
 const PORT = process.env.PORT || 5000;
 
